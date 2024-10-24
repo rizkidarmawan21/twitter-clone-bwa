182a4573d6b930444fe9e530ca7a87739744215b
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2024-10-24 13:35:31 +0700

    feat: signin page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ module.exports = {
     "./assets/css/tailwind.css",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        xl: "75px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      colors: {
+        main: "#010003",
+        secondary: "#121316",
+        blue: {
+          DEFAULT: "#1880E8",
+        },
+        grey: {
+          DEFAULT: "#5E6368",
+        },
+        red: {
+          DEFAULT: "#F91880",
+        },
+        title: "#FFFFFF",
+        subtitle: "#5E6368",
+      },
+    },
   },
   plugins: [],
 }
