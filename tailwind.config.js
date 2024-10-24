5faa1c99e3e72285de4a4c9e73a549a5a7eb7fc7
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2024-10-24 10:25:16 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}", // all html and js files in the root directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
